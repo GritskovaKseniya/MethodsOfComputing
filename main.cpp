8670327f8d792909c527ddefc60be5f0e2987bd6
Kseniya <grickovaKS@gmail.com>
2020-04-27 13:54:36 +0300

    first attempt to surrender

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ using namespace std;
 int main(){
     double q, eps, err;
     cout << "Input q: " << endl;
-    cin >> q; //q = 1 
-    cout << "Input epsilon: " << endl; // должна не сильно отличаться от 0
+    cin >> q; //q = 1 from calculations
+    cout << "Input epsilon: " << endl; // shouldn't differ much from 0
     cin >> eps;
     vector<double> x_cur(3,0);
     vector<double> x_prev;
@@ -17,22 +17,18 @@ int main(){
     {
         x_prev = x_cur;
         x_cur = F(x_cur);
-        // вычисляем погрешность
-        err = calc_error(x_prev, x_cur, eps, q); // погрешность
+        //calculate error
+        err = calc_error(x_prev, x_cur, q);
+        cout << "X=";
+        print(x_cur);
+        cout << "ERROR: " << err << endl;
 
     } while (eps<= err);
 
-    // выводим на экран
-    cout << "X=";
-    for (int i = 0; i < x_cur.size(); i++) {
-        cout << x_cur[i] << " ";
-    }
-    cout << endl;
-    cout << "Error:" << err << endl;
+    
     pr = F(x_cur);
-    cout << "Vector proverki: ";
-    for (int i = 0; i < pr.size(); i++) {
-        cout << pr[i] << " ";
-    }
+    cout << "Check vector: ";
+    print(pr);
     cout << endl;
 }
+
--- utils.cpp
+++ utils.cpp
@@ -5,29 +5,32 @@
 
 using namespace std;
 
-double calc_error(vector<double> x, vector<double> y, double eps, double q){
-    /*cout << "X = ";
-    for (int i = 0; i < x.size(); i++) {
-            cout << x[i] << " ";
-        }
-    cout << endl;
-    cout << "Y = ";
-    for (int i = 0; i < y.size(); i++) {
-            cout << y[i] << " ";
-        }
-    cout << endl;*/
-    if(sqrt((y[0] - x[0])*(y[0] - x[0]) + (y[1] - x[1])*(y[1] - x[1]) + (y[2] - x[2])*(y[2] - x[2]) ) <= eps*(1-q)/q){
-        return sqrt((y[0] - x[0])*(y[0] - x[0]) + (y[1] - x[1])*(y[1] - x[1]) + (y[2] - x[2])*(y[2] - x[2]) );
+double calc_error(vector<double> x, vector<double> y, double q){
+    double err; // variable error
+    double t = 0;
+    for(int i = 0; i < x.size(); i++){
+        double d = abs(x[i] - y[i]);
+        t += d*d;
     }
+    err = q/(1-q) * sqrt(t);
+    return err;
 }
 
 vector<double> F(vector<double> x){
     vector<double> result(3,0);
 //    result.push_back( ( sqrt(5) - atan(x[0] - x[1]) ) / 3 );
-
+    //vector values from calculations
     result[0] = ( sqrt(3) - ( x[1]* ( cos(x[2])* cos(x[2]) ) )  / 
                         ( x[1]*x[1] + 1) ) / 2;
     result[1] = ( 14 - exp( (-1)*abs(x[1]) ) - exp( (-1)*(pow(x[2], 2) ) ) ) / 5;
     result[2] = ( sqrt(5) - atan(x[0] - x[1]) ) / 3;
     return result;
+}
+
+void print(vector<double> x){
+    //print on screen
+    for (int i = 0; i < x.size(); i++) {
+        cout << x[i] << " ";
+    }
+    cout << endl;
 }
